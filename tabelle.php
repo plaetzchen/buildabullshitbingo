78abb38407bf6e1909788b6205a9dce1604ff256
Philip Brechler <pbrechler@mac.com>
2011-03-27 23:36:22 +0200

    Post eingebaut

--- tabelle.php
+++ tabelle.php
@@ -9,31 +9,38 @@
 <body>
 <center>
  
-<? 
+<?php 
 
-$daten = explode("\n",$_GET['daten']); 
+$daten = explode("\n",$_REQUEST['daten']); 
 for($i=0; $i<count($daten); $i++) {
-$daten[$i] = htmlentities($daten[$i]);
+	$daten[$i] = htmlentities($daten[$i]);
 }
 
-$pages = $_GET['pages'];
+$pages = $_REQUEST['pages'];
 $pages = intval($pages);
 
-for($d=0; $d<$pages; $d++){ 
-	shuffle($daten);
-	if($d == 0){
-echo "<table border='1'>";
-	}
-	else {
-	echo "<p style='page-break-before:always'><table border='1'>";	
-	}
-for($i=0; $i<20; $i=$i+4) 
+for($d=0; $d<$pages; $d++)
 { 
-echo "<tr height='100'>"; 
-echo "<td width='100' align='center'>" . $daten[$i] . "</td><td width='100' align='center'>" . $daten[$i+1] . "</td><td width='100' align='center'>" . $daten[$i+2] . "</td><td width='100' align='center'>" . $daten[$i+3] . "</td><td width='100' align='center'>" . $daten[$i+4] ."</td>"; 
-echo "</tr>";
-} 
-echo "</table><br><br><br></p>";
+	shuffle($daten);
+
+	if($d == 0) {
+		echo "<table border='1'>";
+	} else {
+		echo "<p style='page-break-before:always'><table border='1'>";	
+	}
+	
+	for($i=0; $i<20; $i=$i+4) 
+	{ 
+		echo "<tr height='100'>"; 
+		echo "<td width='100' align='center'>" . 
+				$daten[$i] . "</td><td width='100' align='center'>" . 
+				$daten[$i+1] . "</td><td width='100' align='center'>" . 
+				$daten[$i+2] . "</td><td width='100' align='center'>" . 
+				$daten[$i+3] . "</td><td width='100' align='center'>" . 
+				$daten[$i+4] ."</td>"; 
+		echo "</tr>";
+	} 
+	echo "</table><br><br><br></p>";
 }
 ?> 
 
