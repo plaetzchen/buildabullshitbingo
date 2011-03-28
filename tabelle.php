6d31f7e317f5f937695e7cb6ef89361e41dcd146
Matthias Vill <matthias@vill.de>
2011-03-28 03:36:36 +0200

    Kleiner Bug-Fix & Cleanup
    - Nicht vorhandenes Icon auskommentiert
    - Nicht vorhandenes Css auskommentiert
    - Zeilen/Spalten-Zahl konfigurierbar gemacht
    - Doppeltes Wort Zeilenanfang/-ende entfernt
    - Code-Doppelungen reduziert
    - Ein/Zwei Formatierungen
    - Funktionen zum Aufbereiten der Nutzereingaben
    - Leere Zeilen werden unterdrückt

--- tabelle.php
+++ tabelle.php
@@ -1,50 +1,72 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-15">
-<title></title>
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-<link rel="stylesheet" type="text/css" href=".css"/>
+	<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-15">
+	<title></title>
+	<!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/> -->
+	<!-- <link rel="stylesheet" type="text/css" href=".css"/> -->
 </head>
+
 <body>
-<center>
  
 <?php 
+// ---------------------------------- Config ----------------------------------
 
-$daten = explode("\n",$_REQUEST['daten']); 
-for($i=0; $i<count($daten); $i++) {
-	$daten[$i] = htmlentities($daten[$i]);
+$field_size = 5;
+
+// --------------------------- User Input Functions ---------------------------
+
+function prep_data($input) {
+	return htmlentities(strip_magic($input));
 }
 
-$pages = $_REQUEST['pages'];
-$pages = intval($pages);
+function strip_magic($input) {
+	if (get_magic_quotes_gpc()) {
+		return stripslashes_deep($input);
+	} else {
+		return $input;
+	}
+}
 
-for($d=0; $d<$pages; $d++)
-{ 
+function stripslashes_deep($value) {
+	if(is_array($value)) {
+		return array_map('stripslashes_deep', $value);
+	} else {
+		return stripslashes($value);
+	}
+}
+
+// ----------------------------------- Main -----------------------------------
+
+$daten = preg_split("/[\r\n]+/", $_REQUEST['daten'], -1, PREG_SPLIT_NO_EMPTY);
+$daten = array_map('prep_data', $daten);
+
+$pages = intval($_REQUEST['pages']);
+
+for($d=0; $d < $pages; $d++) { 
 	shuffle($daten);
 
-	if($d == 0) {
-		echo "<table border='1'>";
-	} else {
-		echo "<p style='page-break-before:always'><table border='1'>";	
+	if($d != 0) {
+		echo "<p style='page-break-before:always'>";
 	}
+	echo "<table border='1' align='center'>";
 	
-	for($i=0; $i<20; $i=$i+4) 
-	{ 
-		echo "<tr height='100'>"; 
-		echo "<td width='100' align='center'>" . 
-				$daten[$i] . "</td><td width='100' align='center'>" . 
-				$daten[$i+1] . "</td><td width='100' align='center'>" . 
-				$daten[$i+2] . "</td><td width='100' align='center'>" . 
-				$daten[$i+3] . "</td><td width='100' align='center'>" . 
-				$daten[$i+4] ."</td>"; 
+	for($i=0; $i < $field_size; $i++) { 
+		echo "<tr height='100'>";
+		for($j = 0; $j < $field_size; $j++) { 
+			echo "<td width='100' align='center'>";
+			echo $daten[$i * $field_size + $j];
+			echo "</td>";
+		}
 		echo "</tr>";
 	} 
-	echo "</table><br><br><br></p>";
+	echo "</table><br><br><br>";
+	
+	if($d != 0) {
+		echo "</p>";
+	}
 }
-?> 
-
-</center>
+?>
 
 </body> 
 </html>
